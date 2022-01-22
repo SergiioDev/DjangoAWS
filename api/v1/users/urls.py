8f076ff9a0d1ab019ef85ac25504b5e2ca0b4669
SergiioDev <sergi13456@hotmail.es>
2022-01-22 20:10:52 +0100

    create the endpoints for the users

--- api/v1/users/urls.py
+++ api/v1/users/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.UserListCreate.as_view())
+    path('', views.UserList.as_view()),
+    path('<int:pk>', views.UserRetrieveUpdateDestroy.as_view()),
+    path('register', views.UserRegister.as_view())
 ]
