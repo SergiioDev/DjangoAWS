ba3c9a3dc28ed64b61fc61c02ffcd6b64177340f
SergiioDev <sergi13456@hotmail.es>
2022-01-22 20:06:48 +0100

    user serializer created

--- api/v1/users/serializer.py
+++ api/v1/users/serializer.py
@@ -1,18 +1,49 @@
-from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
+from rest_framework import serializers, status
+from rest_framework.response import Response
+from rest_framework.validators import UniqueValidator
+
 from users.models import User
+from organizations.models import Organization
 
 
 class UserSerializer(serializers.ModelSerializer):
+    organization = serializers.PrimaryKeyRelatedField(queryset=Organization.objects.all(), required=False)
+
     class Meta:
         model = User
-        fields = ('email', 'username', 'first_name', 'password', 'organization', 'is_staff', 'is_active')
+        fields = ('id', 'email', 'username', 'first_name', 'password', 'organization', 'is_staff', 'is_active')
         extra_kwargs = {'password': {'write_only': True}}
         depth = 1
 
+
+class RegisterSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    organization = serializers.PrimaryKeyRelatedField(queryset=Organization.objects.all(), required=False)
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'password2', 'email', 'first_name', 'organization')
+        depth = 1
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"status": "Failed",
+                                               "message": "Password fields didn't match."})
+        return attrs
+
     def create(self, validated_data):
-        password = validated_data.pop('password', None)
-        instance = self.Meta.model(**validated_data)
-        if password is not None:
-            instance.set_password(password)
-            instance.save()
-        return instance
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+        )
+
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
