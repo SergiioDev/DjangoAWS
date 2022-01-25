f1641df6885e8038dde2cb6d6a8079f6f57d4225
SergiioDev <sergi13456@hotmail.es>
2022-01-25 09:59:16 +0100

    fix url format and the return json in retrieve an organization by id

--- api/v1/organizations/urls.py
+++ api/v1/organizations/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.OrganizationListCreate.as_view()),
-    path('<int:pk>', views.OrganizationRetrieveUpdateDestroy.as_view())
+    path('<int:pk>/', views.OrganizationRetrieveUpdateDestroy.as_view())
 ]
