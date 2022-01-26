cdf476fcd450f4d7219d18c6fa90e0262a5d91c1
SergiioDev <sergi13456@hotmail.es>
2022-01-26 12:15:55 +0100

    register users and organizations to the admin page

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+
+class UserAdminConfig(UserAdmin):
+    model = User
+    search_fields = ('email', 'username', 'first_name', 'organization', 'position')
+    list_filter = ('email', 'username', 'first_name', 'is_active', 'is_staff', 'organization', 'position')
+    ordering = ('-start_date', )
+    list_display = ('email', 'username', 'first_name',
+                    'is_active', 'is_staff', 'organization', 'position')
+    fieldsets = (
+        (None, {'fields': ('email', 'username', 'first_name', 'organization', 'position')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'first_name', 'password1', 'password2', 'is_active', 'is_staff',  'organization', 'position')
+        }),
+    )
+
+
+admin.site.register(User, UserAdminConfig)
